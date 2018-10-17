b6d9443e6a7735492026916c7275726fdf505bff
Mike Harder <mharder@microsoft.com>
2018-10-17 10:58:22 -0700

    Sample projects and build pipeline

---
+++ KeyVault/Microsoft.Adp.KeyVault.Test/KeyVaultUriTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace Microsoft.Adp.KeyVault.Test
+{
+    public class KeyVaultUriTest
+    {
+        [Test]
+        public void ConstructorTest() {
+            Assert.NotNull(new KeyVaultUri());
+        }
+    }
+}
---
+++ KeyVault/Microsoft.Adp.KeyVault/KeyVaultUri.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Microsoft.Adp.KeyVault
+{
+    public class KeyVaultUri
+    {
+    }
+}
