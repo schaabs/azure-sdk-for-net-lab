ddfc24eddd7974d7f6d14f3fb6c56249dca9cc56
Scott Schaab <sschaab@microsoft.com>
2019-01-16 18:04:44 -0800

    creating prototype skeleton

---
+++ KeyVault/Azure.Security.KeyVault/KeyVaultClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Azure.Core;
+using Azure.Core.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Security.KeyVault
+{
+    public class KeyVaultClient
+    {
+        
+        const string SdkName = "Azure.Security.KeyVault";
+        
+        const string SdkVersion = "1.0.0";
+
+        public async Task<Response<>>
+    }
+}
---
+++ KeyVault/Azure.Security.KeyVault/Secret.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+namespace Azure.Security.KeyVault
+{
+    public sealed class Secret
+    {
+        
+        public string Id { get; set; }
+
+        public string Value { get; set; }
+
+        public string ContentType { get; set; }
+
+        public 
+    }
+}
