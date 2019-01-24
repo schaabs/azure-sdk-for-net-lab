5c0ae45bcd74d061bdf2fcc3de100e7ce43ac849
Scott Schaab <sschaab@microsoft.com>
2019-01-24 00:01:28 -0800

    utf8reader serialization incomplete

--- KeyVault/Azure.Security.KeyVault/Secret.cs
+++ KeyVault/Azure.Security.KeyVault/Secret.cs
@@ -1,5 +1,8 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
 
 namespace Azure.Security.KeyVault
 {
@@ -21,14 +24,6 @@ namespace Azure.Security.KeyVault
 
         public void Serialize(Utf8JsonWriter2 writer, ReadOnlySpan<byte> propertyName = null)
         {
-            if(string.IsNullOrEmpty(suppressObject))
-            {
-                writer.WriteStartObject();
-            }
-            else
-            {
-                writer.WriteStartObject(propertyName);
-            }
 
             if (Id != null)
             {
@@ -54,8 +49,6 @@ namespace Azure.Security.KeyVault
                     writer.writeString(tag.Key, tag.Value);
                 }
             }
-
-            writer.WriteEndObject();
         }
 
         public void Deserialize(Utf8Json.Reader reader)
@@ -102,37 +95,152 @@ namespace Azure.Security.KeyVault
         }
     }
 
-    [System.AttributeUsage(System.AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+    internal abstract class Model
+    {
+        private static JsonAttribute[] s_jsonProperties;
+        private static Object s_initLock = new Object();
+        private static bool s_init = false;
+
+        public Model()
+        {
+            InitializeJsonProperties();
+        }
+
+        private void InitializeJsonProperties()
+        {
+            if (!s_init)
+            {
+                lock(s_initLock)
+                {
+                    if(!s_init)
+                    {
+                        // get the JsonAttribute for all public properties on the current type
+                        // filter out properties without the attribute or attributes with Ignored set
+                        // sort the attributes by Utf8Name so the properties can be looked up with binary search
+                        // when deserializing
+                        s_jsonProperties = this.GetType().GetProperties()
+                                                         .Select( prop => prop.GetCustomAttributes<JsonAttribute>().FirstOrDefault())
+                                                         .Where(attr => attr != null && !attr.Ignored)
+                                                         .OrderBy(attr => attr.Utf8Name, ByteSpanComparer.Singleton);
+                        s_init = true;
+                    }
+                }
+            }
+        }
+
+        public void Deserialize(Utf8JsonReader reader)
+        {
+            // ensure that the begining of the json text is startObject
+            if (!reader.Read() || reader.JsonTokenType != JsonTokenType.StartObject)
+            {
+                throw new SerializationException();
+            }
+
+            // read until the end of the object
+            while (reader.Read() && reader.TokenType != JsonTokenType.EndObject)
+            {
+                // the first token in each loop iteration should be a property
+                if (reader.JsonTokenType != JsonTokenType.PropertyName)
+                {
+                    throw new SerializationException();
+                }
+
+
+            }
+        }
+
+        public void Serialize(Utf8JsonWriter2 writer, ReadOnlySpan<byte> propertyName = null)
+        {
+
+            if(string.IsNullOrEmpty(propertyName))
+            {
+                writer.WriteStartObject();
+            }
+            else
+            {
+                writer.WriteStartObject(propertyName);
+            }
+            
+            for (int i = 0; i < s_jsonProperties.Length; i++)
+            {
+                WriteProperty(writer, s_jsonProperties[i]);
+            }
+
+            writer.WriteEndObject();
+        }
+
+        // Making this an abstract method for now so that individual classes can explicitly write properties
+        // without the need for reflection.  If we determin that the performance hit of reflection is acceptable
+        // this could be implemented here.
+        protected abstract void WriteProperty(Utf8JsonWriter writer, JsonAttribute attr);
+
+        // Making this an abstract method for now so that individual classes can explicitly read properties
+        // without the need for reflection.  If we determin that the performance hit of reflection is acceptable
+        // this could be implemented here.
+        protected abstract void ReadProperty(Utf8JsonReader reader, JsonAttribute attr);
+    }
+
+    [System.AttributeUsage(System.AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     sealed class JsonAttribute : System.Attribute
     {
         // See the attribute guidelines at
         //  http://go.microsoft.com/fwlink/?LinkId=85236
-        readonly string _propertyName;
-        readonly ReadOnlySpan<byte> _utf8PropertyName;
+        readonly string _name;
+        readonly ReadOnlySpan<byte> _utf8Name;
         readonly bool _ignored;
+        readonly int _id;
         
         // This is a positional argument
-        public JsonAttribute(string propertyName = null)
+        public JsonAttribute(int id = 0, string name = null, bool ignored = false)
         {
-            this._propertyName = propertyName;
-            
-            // TODO: Implement code here
-            throw new System.NotImplementedException();
+            _name = name;
+            _id = id;
+            _ignored = ignored;         
+
         }
         
-        public string PropertyName
+        public int Id
         {
-            get { return _propertyName; }
+            get { return _id; }
         }
 
-        public ReadOnlySpan<byte> Utf8PropertyName
+        public string Name
         {
-            get { return _utf8PropertyName; }
+            get { return _name; }
+        }
+
+        public ReadOnlySpan<byte> Utf8Name
+        {
+            get { return _utf8Name; }
         }
         
+        public string PropertyName { get; set; }
+
         public bool Ignored
         {
-            get { return }
+            get { return _ignored; }
+        }
+    }
+
+    public class ByteSpanComparer : IComparer<ReadOnlySpan<byte>>
+    {
+        public static ByteSpanComparer Singleton = new ByteSpanComparer();
+
+        public int Compare(ReadOnlySpan<byte> x, ReadOnlySpan<byte> y)
+        {
+            int comp = 0;
+
+            for(int i = 0; i < x.Length && i < y.Length && comp == 0; i++)
+            {
+                comp = x[i].CompareTo(y[i]);
+            }
+
+            if (comp == 0)
+            {
+                comp = x.Length.CompareTo(y.Length);
+            }
+
+            return comp;
         }
     }
 
