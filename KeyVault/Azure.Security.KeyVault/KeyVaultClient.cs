7ec4603014678c0979a5c4e045740f67e456ee2a
Scott Schaab <sschaab@microsoft.com>
2019-01-25 11:21:34 -0800

    client updates

--- KeyVault/Azure.Security.KeyVault/KeyVaultClient.cs
+++ KeyVault/Azure.Security.KeyVault/KeyVaultClient.cs
@@ -14,7 +14,29 @@ namespace Azure.Security.KeyVault
         private const string SdkVersion = "1.0.0";
 
         private const string secretRoute = "/secrets/";
-        public async Task<Response<Secret>> SetSecretAsync(string name, string value, string contentType = null, IDictionary<string, string> tags = null, VaultObjectAttributes attributes = null, CancellationToken cancellation = default)
+
+        private readonly TokenCredential _credentials;
+        
+        private readonly PipelineOptions _options;
+
+        private readonly HttpPipeline _pipeline;
+
+        public KeyVaultClient(TokenCredential credentials)
+            : this (credentials, new PipelineOptions())
+        {
+
+        }
+
+        public KeyVaultClient(TokenCredential credentials, PipelineOptions options)
+        {
+            if (credentials == null) throw new ArgumentNullException(nameof(credentials));
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            _options = options;
+            _pipeline = HttpPipeline.Create(_options, SdkName, SdkVersion);
+        }
+
+        public async Task<Response<Secret>> SetSecretAsync(string name, string value, string contentType = null, VaultObjectAttributes attributes = null, IDictionary<string, string> tags = null, CancellationToken cancellation = default)
         {
             if(string.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name));
             if(string.IsNullOrEmpty(value)) throw new ArgumentNullException(nameof(value));
@@ -25,6 +47,22 @@ namespace Azure.Security.KeyVault
             {
                 message.SetRequestLine(PipelineMethod.Put, secretUri);
                 message.AddHeader("Host", uri.Host);
+                message.AddHeader("Accept", "application/json");
+                message.AddHeader("Content-Type", "application/json; charset=utf-8");
+                message.AddHeader("Authorization", "Bearer " + _credentials.GetToken());
+
+                var secret = new Secret() 
+                { 
+                    Value = value, 
+                    ContentType = contentType,
+                    Attributes = attributes,
+                    Tags = tags
+                };
+
+                message.SetContent(secret.Serialize());
+
+                // todo: shouldn't this take in the cancellation token
+                await _pipeline.ProcessAsync(message);
             }
 
 
