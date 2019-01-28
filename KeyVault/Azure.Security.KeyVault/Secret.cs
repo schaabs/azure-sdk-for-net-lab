cc66e249f1a24097b7628f9aa56438b25a668e1e
Scott Schaab <sschaab@microsoft.com>
2019-01-28 09:19:09 -0800

    utf8reader/write serialization updates

--- KeyVault/Azure.Security.KeyVault/Secret.cs
+++ KeyVault/Azure.Security.KeyVault/Secret.cs
@@ -2,100 +2,301 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
 using System.Text.Json;
 
 namespace Azure.Security.KeyVault
 {
-    public sealed class Secret
-    {        
-        public string Id { get; set; }
-
-        public string Value { get; set; }
-
-        public string ContentType { get; set; }
-
-        public SecretAttributes Attributes { get; set; }
-
-        public IDictionary<string, string> Tags { get; set; }
-
-        public string Kid { get; private set; }
-
-        public bool? Managed { get; private set; }
-
-        public void Serialize(Utf8JsonWriter2 writer, ReadOnlySpan<byte> propertyName = null)
+    internal static class JsonExtensions
+    {
+        internal static IDictionary<string, string> GetStringDictionary(this Utf8JsonReader reader)
         {
+            reader.Read();
 
-            if (Id != null)
-            {
-                writer.WriteString("id", Id);
-            }
-
-            if (Value != null)
-            {
-                writer.WriteString("value", Value);
-            }
-
-            if (ContentType != null)
-            {
-                writer.WriteString("contentType", ContentType);
-            }
-
-            if (Tags != null)
-            {
-                writer.WriteStartObject("tags");
-                
-                foreach(var tag in Tags)
-                {
-                    writer.writeString(tag.Key, tag.Value);
-                }
-            }
-        }
-
-        public void Deserialize(Utf8Json.Reader reader)
-        {
-            if (!reader.Read() || reader.JsonTokenType != JsonTokenType.StartObject)
+            if (reader.TokenType != JsonTokenType.StartObject)
             {
                 throw new SerializationException();
             }
 
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
             while (reader.Read() && reader.TokenType != JsonTokenType.EndObject)
             {
-                if (reader.JsonTokenType != JsonTokenType.PropertyName)
-                {
-                    throw new SerializationException();
-                }
+                var key = reader.GetString();
 
+                reader.Read();
 
+                dict[key] = reader.GetString();
             }
-        }
-    }
 
-    public abstract class SecretAttributes
-    {
-        public bool? Enabled { get; set; }
-        public DateTime? Created { get; set; }
-                
-        public int Serialize(Utf8JsonWriter2 writer, string propertyName = null)
+            return dict;
+        }
+
+        internal static void WriteStringDictionary(this Utf8JsonWriter writer, ReadOnlySpan<byte> propertyName, IDictionary<string, string> dictionary)
         {
-            if(string.IsNullOrEmpty(suppressObject))
-            {
-                writer.WriteStartObject();
-            }
-            else
-            {
-                writer.WriteStartObject(propertyName);
-            }
-            
-            writer.WriteString("id", Id);
-            writer.WriteString("value", Value);
-            writer.WriteString("contentType", ContentType);
-            writer.WriteStartObject("attributes");
-            writer.WriteEndObject();
+            writer.WriteStartObject(propertyName);
 
+            foreach (var kvp in dictionary)
+            {
+                writer.WriteString(kvp.Key, kvp.Value);
+            }
+
+            writer.WriteEndObject();
+        }
+
+        internal static DateTime GetUnixMillisecondTimestamp(this Utf8JsonReader reader)
+        {
+            return DateTimeOffset.FromUnixTimeMilliseconds(reader.GetInt64()).DateTime;
+        }
+
+        internal static void WriteUnixMillisecondTimestamp(this Utf8JsonWriter writer, ReadOnlySpan<byte> propertyName, DateTime datetime)
+        {
+            writer.WriteNumber(propertyName, new DateTimeOffset(datetime).ToUnixTimeMilliseconds());
         }
     }
 
-    internal abstract class Model
+    public sealed class Secret : Model
+    {
+        private const int IdPropId = 1;
+        private const int ValuePropId = 2;
+        private const int ContentTypePropId = 3;
+        private const int AttributesPropId = 4;
+        private const int TagsPropId = 5;
+        private const int KidPropId = 6;
+        private const int ManagedPropId = 7;
+
+        [Json("id", IdPropId)]
+        public string Id { get; set; }
+
+        [Json("value", ValuePropId)]
+        public string Value { get; set; }
+        
+        [Json("contenType", ContentTypePropId)]
+        public string ContentType { get; set; }
+
+        [Json("attributes", AttributesPropId)]
+        public VaultObjectAttributes Attributes { get; set; }
+
+        [Json("tags", TagsPropId)]
+        public IDictionary<string, string> Tags { get; set; }
+
+        [Json("kid", KidPropId)]
+        public string Kid { get; private set; }
+
+        [Json("managed", ManagedPropId)]
+        public bool? Managed { get; private set; }
+
+        internal override void ReadProperty(Utf8JsonReader reader, JsonAttribute attr)
+        {
+            switch (attr.Id)
+            {
+                case IdPropId:
+                    reader.Read();
+                    Id = reader.GetString();
+                    break;
+                case ValuePropId:
+                    reader.Read();
+                    Value = reader.GetString();
+                    break;
+                case ContentTypePropId:
+                    reader.Read();
+                    ContentType = reader.GetString();
+                    break;
+                case AttributesPropId:
+                    Attributes = new VaultObjectAttributes();
+                    Attributes.Deserialize(reader);
+                    break;
+                case TagsPropId:
+                    Tags = reader.GetStringDictionary();
+                    break;
+                case KidPropId:
+                    reader.Read();
+                    Kid = reader.GetString();
+                    break;
+                case ManagedPropId:
+                    reader.Read();
+                    Managed = reader.GetBoolean();
+                    break;
+                default:
+                    throw new SerializationException();
+            }
+        }
+
+        internal override void WriteProperty(Utf8JsonWriter writer, JsonAttribute attr)
+        {
+            switch (attr.Id)
+            {
+                case IdPropId:
+                    if (Id != null)
+                    {
+                        writer.WriteString(attr.Utf8Name, Id);
+                    }
+                    break;
+                case ValuePropId:
+                    if (Value != null)
+                    {
+                        writer.WriteString(attr.Utf8Name, Value);
+                    }
+                    break;
+                case ContentTypePropId:
+                    if (ContentType != null)
+                    {
+                        writer.WriteString(attr.Utf8Name, ContentType);
+                    }
+                    break;
+                case AttributesPropId:
+                    if (Attributes != null)
+                    {
+                        Attributes.Serialize(writer, attr.Utf8Name);
+                    }
+                    break;
+                case TagsPropId:
+                    if(Tags != null)
+                    {
+                        writer.WriteStringDictionary(attr.Utf8Name, Tags);
+                    }
+                    break;
+                case KidPropId:
+                    if (Kid != null)
+                    {
+                        writer.WriteString(attr.Utf8Name, Kid);
+                    }
+                    break;
+                case ManagedPropId:
+                    if (Managed != null)
+                    {
+                        writer.WriteBoolean(attr.Utf8Name, Managed.Value);
+                    }
+                    break;
+                default:
+                    throw new SerializationException();
+            }
+        }
+    }
+
+    public sealed class VaultObjectAttributes : Model
+    {
+        private const int EnabledPropId = 1;
+        private const int NotBeforePropId = 2;
+        private const int ExpiresPropId = 3;
+        private const int CreatedPropId = 4;
+        private const int UpdatedPropId = 5;
+        private const int RecoveryLevelPropId = 6;
+
+        public bool? Enabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets not before date in UTC.
+        /// </summary>
+        public System.DateTime? NotBefore { get; set; }
+
+        /// <summary>
+        /// Gets or sets expiry date in UTC.
+        /// </summary>
+        public System.DateTime? Expires { get; set; }
+
+        /// <summary>
+        /// Gets creation time in UTC.
+        /// </summary>
+        public System.DateTime? Created { get; private set; }
+
+        /// <summary>
+        /// Gets last updated time in UTC.
+        /// </summary>
+        public System.DateTime? Updated { get; private set; }
+
+        /// <summary>
+        /// Gets reflects the deletion recovery level currently in effect for
+        /// secrets in the current vault. If it contains 'Purgeable', the
+        /// secret can be permanently deleted by a privileged user; otherwise,
+        /// only the system can purge the secret, at the end of the retention
+        /// interval. Possible values include: 'Purgeable',
+        /// 'Recoverable+Purgeable', 'Recoverable',
+        /// 'Recoverable+ProtectedSubscription'
+        /// </summary>
+        public string RecoveryLevel { get; private set; }
+
+        internal override void ReadProperty(Utf8JsonReader reader, JsonAttribute attr)
+        {
+            switch (attr.Id)
+            {
+                case EnabledPropId:
+                    reader.Read();
+                    Enabled = reader.GetBoolean();
+                    break;
+                case NotBeforePropId:
+                    reader.Read();
+                    NotBefore = reader.GetUnixMillisecondTimestamp();
+                    break;
+                case ExpiresPropId:
+                    reader.Read();
+                    Expires = reader.GetUnixMillisecondTimestamp();
+                    break;
+                case CreatedPropId:
+                    reader.Read();
+                    Created = reader.GetUnixMillisecondTimestamp();
+                    break;
+                case UpdatedPropId:
+                    reader.Read();
+                    Updated = reader.GetUnixMillisecondTimestamp();
+                    break;
+                case RecoveryLevelPropId:
+                    reader.Read();
+                    RecoveryLevel = reader.GetString();
+                    break;
+                default:
+                    throw new SerializationException();
+            }
+        }
+
+        internal override void WriteProperty(Utf8JsonWriter writer, JsonAttribute attr)
+        {
+            switch(attr.Id)
+            {
+                case EnabledPropId:
+                    if(Enabled != null)
+                    {
+                        writer.WriteBoolean(attr.Utf8Name, Enabled.Value);
+                    }
+                    break;
+                case NotBeforePropId:
+                    if(NotBefore != null)
+                    {
+                        writer.WriteUnixMillisecondTimestamp(attr.Utf8Name, NotBefore.Value);
+                    }
+                    break;
+                case ExpiresPropId:
+                    if (Expires != null)
+                    {
+                        writer.WriteUnixMillisecondTimestamp(attr.Utf8Name, Expires.Value);
+                    }
+                    break;
+                case CreatedPropId:
+                    if (Created != null)
+                    {
+                        writer.WriteUnixMillisecondTimestamp(attr.Utf8Name, Created.Value);
+                    }
+                    break;
+                case UpdatedPropId:
+                    if (Updated != null)
+                    {
+                        writer.WriteUnixMillisecondTimestamp(attr.Utf8Name, Updated.Value);
+                    }
+                    break;
+                case RecoveryLevelPropId:
+                    if (RecoveryLevel != null)
+                    {
+                        writer.WriteString(attr.Utf8Name, RecoveryLevel);
+                    }
+                    break;
+                default:
+                    throw new SerializationException();
+            }
+        }
+    }
+
+    public abstract class Model
     {
         private static JsonAttribute[] s_jsonProperties;
         private static Object s_initLock = new Object();
@@ -115,44 +316,20 @@ namespace Azure.Security.KeyVault
                     if(!s_init)
                     {
                         // get the JsonAttribute for all public properties on the current type
-                        // filter out properties without the attribute or attributes with Ignored set
-                        // sort the attributes by Utf8Name so the properties can be looked up with binary search
-                        // when deserializing
+                        // filter out properties without the attribute
                         s_jsonProperties = this.GetType().GetProperties()
-                                                         .Select( prop => prop.GetCustomAttributes<JsonAttribute>().FirstOrDefault())
-                                                         .Where(attr => attr != null && !attr.Ignored)
-                                                         .OrderBy(attr => attr.Utf8Name, ByteSpanComparer.Singleton);
+                                                         .Select(prop => prop.GetCustomAttributes<JsonAttribute>().FirstOrDefault())
+                                                         .Where(attr => attr != null).ToArray();
                         s_init = true;
                     }
                 }
             }
         }
+        
 
-        public void Deserialize(Utf8JsonReader reader)
+        internal void Serialize(Utf8JsonWriter writer, ReadOnlySpan<byte> propertyName = default)
         {
-            // ensure that the begining of the json text is startObject
-            if (!reader.Read() || reader.JsonTokenType != JsonTokenType.StartObject)
-            {
-                throw new SerializationException();
-            }
-
-            // read until the end of the object
-            while (reader.Read() && reader.TokenType != JsonTokenType.EndObject)
-            {
-                // the first token in each loop iteration should be a property
-                if (reader.JsonTokenType != JsonTokenType.PropertyName)
-                {
-                    throw new SerializationException();
-                }
-
-
-            }
-        }
-
-        public void Serialize(Utf8JsonWriter2 writer, ReadOnlySpan<byte> propertyName = null)
-        {
-
-            if(string.IsNullOrEmpty(propertyName))
+            if(propertyName.Length == 0)
             {
                 writer.WriteStartObject();
             }
@@ -160,7 +337,7 @@ namespace Azure.Security.KeyVault
             {
                 writer.WriteStartObject(propertyName);
             }
-            
+
             for (int i = 0; i < s_jsonProperties.Length; i++)
             {
                 WriteProperty(writer, s_jsonProperties[i]);
@@ -168,16 +345,51 @@ namespace Azure.Security.KeyVault
 
             writer.WriteEndObject();
         }
+        
+        internal void Deserialize(Utf8JsonReader reader)
+        {
+            ulong visited = 0;
+            ulong visiting = 1;
+
+            reader.Read();
+
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                throw new SerializationException();
+            }
+
+            while (reader.Read() && reader.TokenType != JsonTokenType.EndObject)
+            {            
+                if (reader.TokenType != JsonTokenType.PropertyName)
+                {
+                    throw new SerializationException();
+                }
+
+                var utf8PropName = reader.ValueSpan;
+
+                for (int i = 0; i < s_jsonProperties.Length; i++)
+                {
+                    if (((visiting & visited) != 0) && (utf8PropName.SequenceEqual(s_jsonProperties[i].Utf8Name)))
+                    {
+                        visited &= visiting;
+
+                        ReadProperty(reader, s_jsonProperties[i]);
+
+                        break;
+                    }
+                }
+            }
+        }
 
         // Making this an abstract method for now so that individual classes can explicitly write properties
         // without the need for reflection.  If we determin that the performance hit of reflection is acceptable
         // this could be implemented here.
-        protected abstract void WriteProperty(Utf8JsonWriter writer, JsonAttribute attr);
+        internal abstract void WriteProperty(Utf8JsonWriter writer, JsonAttribute attr);
 
         // Making this an abstract method for now so that individual classes can explicitly read properties
         // without the need for reflection.  If we determin that the performance hit of reflection is acceptable
         // this could be implemented here.
-        protected abstract void ReadProperty(Utf8JsonReader reader, JsonAttribute attr);
+        internal abstract void ReadProperty(Utf8JsonReader reader, JsonAttribute attr);
     }
 
     [System.AttributeUsage(System.AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
@@ -186,16 +398,15 @@ namespace Azure.Security.KeyVault
         // See the attribute guidelines at
         //  http://go.microsoft.com/fwlink/?LinkId=85236
         readonly string _name;
-        readonly ReadOnlySpan<byte> _utf8Name;
-        readonly bool _ignored;
+        readonly byte[] _utf8Name;
         readonly int _id;
         
         // This is a positional argument
-        public JsonAttribute(int id = 0, string name = null, bool ignored = false)
+        public JsonAttribute(string name, int id = 0)
         {
             _name = name;
             _id = id;
-            _ignored = ignored;         
+            _utf8Name = Encoding.UTF8.GetBytes(name);
 
         }
         
@@ -215,33 +426,6 @@ namespace Azure.Security.KeyVault
         }
         
         public string PropertyName { get; set; }
-
-        public bool Ignored
-        {
-            get { return _ignored; }
-        }
+        
     }
-
-    public class ByteSpanComparer : IComparer<ReadOnlySpan<byte>>
-    {
-        public static ByteSpanComparer Singleton = new ByteSpanComparer();
-
-        public int Compare(ReadOnlySpan<byte> x, ReadOnlySpan<byte> y)
-        {
-            int comp = 0;
-
-            for(int i = 0; i < x.Length && i < y.Length && comp == 0; i++)
-            {
-                comp = x[i].CompareTo(y[i]);
-            }
-
-            if (comp == 0)
-            {
-                comp = x.Length.CompareTo(y.Length);
-            }
-
-            return comp;
-        }
-    }
-
 }
